3030237e2fe3c2057d5b3011d0c0f485588db159
ismailbrahimi <ismailbrahimi222000@gmail.com>
2025-04-17 12:05:36 +0200

    Init project C#

---
+++ ModernSoftwareApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using ModernSoftwareApp;
+
+namespace ModernSoftwareApp.Tests
+{
+    public class CalculatorTests
+    {
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            int result = calc.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
